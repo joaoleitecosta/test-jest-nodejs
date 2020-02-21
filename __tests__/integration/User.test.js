6409d34d9bdff409f8a47afafd951eaee36ea892
Joao Leite <joao_leite@hotmail.com.br>
2020-02-21 15:37:35 -0300

    creating duplicate email test

--- __tests__/integration/User.test.js
+++ __tests__/integration/User.test.js
@@ -1,16 +1,41 @@
 import request from 'supertest';
 import app from '../../src/app';
 
+import truncate from '../util/truncate';
+
 describe('User', () => {
+  beforeEach(async () => {
+    await truncate();
+  });
+
   it('should be able to register', async () => {
     const response = await request(app)
       .post('/users')
       .send({
-        name: 'Joao',
-        email: 'joao_leite@hotmail.com',
+        name: 'Joao Leite',
+        email: 'joao_leite@hotmail.com.br',
         password_hash: '12345678',
       });
 
     expect(response.body).toHaveProperty('id');
   });
+
+  // it('shoult not be able to register with duplicated email', async () => {
+  //   await request(app)
+  //     .post('/users')
+  //     .send({
+  //       name: 'Joao',
+  //       email: 'joao_leite@hotmail.com',
+  //       password_hash: '12345678',
+  //     });
+
+  //   const response = await request(app)
+  //     .post('./users')
+  //     .send({
+  //       name: 'Joao',
+  //       email: 'joao_leite@hotmail.com',
+  //       password_hash: '12345678',
+  //     });
+  //   expect(response.status).toBe(400);
+  // });
 });
